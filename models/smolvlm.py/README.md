c3e7ce4824d84a8ea23e414bdb442b3e0aaa5eec
startchat <gaojing850063636@gmail.com>
2025-08-15 17:07:00 +0000

    fix: fix model path

---
+++ models/smolvlm.py/README.md
@@ -0,0 +1,4 @@
+# SmolVLM
+* 直接采用SmolVLM2-256M-Video-Instruct VIT部分
+* LLM换成qwen3系列模型
+
--- models/ustarvlm/config.py
+++ models/ustarvlm/config.py
@@ -5,8 +5,8 @@ class VLMConfig(PretrainedConfig):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.llm_path = r'/home/dataset1/gaojing/llm/Qwen3-SmVL/save/pretrain_0718'
-        self.vision_model_path = r'/home/dataset1/gaojing/models/siglip2-base-patch16-224'
+        self.llm_path = r'/llm/Qwen3-SmVL/save/pretrain_0718'
+        self.vision_model_path = r'/models/siglip2-base-patch16-224'
         self.image_pad_num = 49
         self.freeze_vision_model = True  # 是否冻结视觉模型进行训练
         self.freeze_llm = True # 是否冻结语言模型
